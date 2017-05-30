ffe609c9f7cb86ec3085fab0bc55a0acf0371d81
Shiv K Sah <shiv.sah@aricent.com>
2017-05-30 12:47:40 +0530

    report UI setup

--- cloud/endagaweb/views/dashboard.py
+++ cloud/endagaweb/views/dashboard.py
@@ -912,10 +912,95 @@ class ActivityView(ProtectedView):
             return res_events
 
 
-class ReportView(ProtectedView):
+class CallReportView(ProtectedView):
     """View reports on basis of Network or tower level."""
 
     def get(self, request):
+        print "call sms report"
+        user_profile = UserProfile.objects.get(user=request.user)
+        network = user_profile.network
+        timezone_offset = pytz.timezone(user_profile.timezone).utcoffset(
+            datetime.datetime.now()).total_seconds()
+        # Determine if there has been any activity on the network (if not, we won't
+        # show the graphs).
+        network_has_activity = UsageEvent.objects.filter(
+            network=network).exists()
+        context = {
+            'networks': get_objects_for_user(request.user, 'view_network',
+                                             klass=Network),
+            'user_profile': user_profile,
+            'network_id': network.id,
+            'current_time_epoch': int(time.time()),
+            'timezone_offset': timezone_offset,
+            'network_has_activity': network_has_activity,
+            'report_summary': 'Call and SMS'
+        }
+        template = get_template("dashboard/report/call-report.html")
+        html = template.render(context, request)
+        return HttpResponse(html)
+
+
+class SubscriberReportView(ProtectedView):
+    """View reports on basis of Network or tower level."""
+
+    def get(self, request):
+        print "subcriber report"
+        user_profile = UserProfile.objects.get(user=request.user)
+        network = user_profile.network
+        timezone_offset = pytz.timezone(user_profile.timezone).utcoffset(
+            datetime.datetime.now()).total_seconds()
+        # Determine if there has been any activity on the network (if not, we won't
+        # show the graphs).
+        network_has_activity = UsageEvent.objects.filter(
+            network=network).exists()
+        context = {
+            'networks': get_objects_for_user(request.user, 'view_network',
+                                             klass=Network),
+            'user_profile': user_profile,
+            'network_id': network.id,
+            'current_time_epoch': int(time.time()),
+            'timezone_offset': timezone_offset,
+            'network_has_activity': network_has_activity,
+            'report_summary': 'Call and SMS'
+        }
+        template = get_template("dashboard/report/subscriber-report.html")
+        html = template.render(context, request)
+        return HttpResponse(html)
+
+
+class BillingReportView(ProtectedView):
+    """View reports on basis of Network or tower level."""
+
+    def get(self, request):
+        print "billing report"
+        user_profile = UserProfile.objects.get(user=request.user)
+        network = user_profile.network
+        timezone_offset = pytz.timezone(user_profile.timezone).utcoffset(
+            datetime.datetime.now()).total_seconds()
+        # Determine if there has been any activity on the network (if not, we won't
+        # show the graphs).
+        network_has_activity = UsageEvent.objects.filter(
+            network=network).exists()
+        context = {
+            'networks': get_objects_for_user(request.user, 'view_network',
+                                             klass=Network),
+            'user_profile': user_profile,
+            'network_id': network.id,
+            'current_time_epoch': int(time.time()),
+            'timezone_offset': timezone_offset,
+            'network_has_activity': network_has_activity,
+            'report_summary': 'Call and SMS'
+        }
+        template = get_template("dashboard/report/call-report.html")
+        html = template.render(context, request)
+        return HttpResponse(html)
+
+
+class HealthReportView(ProtectedView):
+    """View reports on basis of Network or tower level."""
+
+    def get(self, request):
+        print "health report"
         user_profile = UserProfile.objects.get(user=request.user)
         network = user_profile.network
         timezone_offset = pytz.timezone(user_profile.timezone).utcoffset(
